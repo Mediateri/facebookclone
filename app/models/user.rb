fda68326db74ebde48c7a621a77509a8c4cf5f39
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-08-20 07:34:19 +0000

    facebook clone

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :first_name, :last_name, presence: true, length: {maximum: 40}
+    validates :email, presence: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    before_validation { email.downcase! }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 },confirmation: true
+    has_many :posts
+end
