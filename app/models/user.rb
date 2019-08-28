10be4303391ee3d959439534238cc367c46e569d
mediateri <ntawukuriryayomeddy1@gmail.com>
2019-08-28 10:12:16 +0000

    fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :first_name, :last_name, presence: true, length: {maximum: 40}
-    validates :email, presence: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :email, presence: true, uniqueness: true, length: {maximum: 200},format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     before_validation { email.downcase! }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 },confirmation: true
